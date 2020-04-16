6886b685dccb67a4ffa67f7b0b4c4cb01cba3891
ALEXSUCHKOV <62739868+ALEXSUCHKOV@users.noreply.github.com>
2020-04-16 08:21:55 +0700

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Class1 class1 = new Class1();
+                    class1.Setup(class1.proigrish);
+            while (!class1.proigrish)
+            {
+                class1.karta_and_pers();
+                class1.Input_Logic();
+                class1.dvigenie();
+                class1.itog();
+                Thread.Sleep(150);
+            }
+            Console.ReadKey();
+        }
+    }
+}
